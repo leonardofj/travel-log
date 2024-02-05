4f91f42e194dcf1fff200a1d4c050ca72b4f84b8
leonardofj <leonardofj@gmail.com>
2024-02-05 20:38:25 +0100

    feat: creating react frontend

---
+++ frontend/src/App.tsx
@@ -0,0 +1,13 @@
+import ResponsiveNavbar from "./components/Navbar";
+
+function App() {
+  return (
+    <div className="App">
+      <ResponsiveNavbar></ResponsiveNavbar>
+      <h1>My Trips</h1>
+      <div className="card"></div>
+    </div>
+  );
+}
+
+export default App;
