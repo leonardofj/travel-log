b7967d9092337c1f28128a43d60d754b0b806a28
leonardofj <leonardofj@gmail.com>
2024-07-12 16:26:05 +0200

    feat: implement data table

--- frontend/src/routes/Cities.tsx
+++ frontend/src/routes/Cities.tsx
@@ -1,9 +1,45 @@
+import { DataTable } from "../components/DataTable";
 import HeaderWithButton from "../components/HeaderWithButton";
+import { createColumnHelper } from "@tanstack/react-table";
+import { cities } from "../data";
+
+export type CityTable = {
+  id: number;
+  name: string;
+  visited: number;
+  country_id: number;
+  lat: number;
+  lon: number;
+  state: string;
+};
+
+const data: CityTable[] = cities;
+
+const columnHelper = createColumnHelper<CityTable>();
+
+const columns = [
+  columnHelper.accessor("name", {
+    cell: (info) => info.getValue(),
+    header: "City",
+  }),
+  columnHelper.accessor("country_id", {
+    cell: (info) => info.getValue(),
+    header: "Country ID",
+  }),
+  columnHelper.accessor("state", {
+    cell: (info) => info.getValue(),
+    header: "State",
+    meta: {
+      isNumeric: true,
+    },
+  }),
+];
 
 const Cities = () => {
   return (
     <div>
       <HeaderWithButton title={"Cities"}></HeaderWithButton>
+      <DataTable columns={columns} data={data} />
     </div>
   );
 };
