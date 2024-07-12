2662ad4465ce06b2bd4e36bac6867f8e5f0cc5e9
leonardofj <leonardofj@gmail.com>
2024-07-12 18:03:30 +0200

    feat: renaming project

--- backend/travel_log/asgi.py
+++ backend/travel_log/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for trips project.
+ASGI config for travel_log project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "trips.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "travel_log.settings")
 
 application = get_asgi_application()
